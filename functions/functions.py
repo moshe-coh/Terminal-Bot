2ed4a9b9f54db65b3ebca3d67aaa27a8c15c4a53
moshe-coh <62907797+moshe-coh@users.noreply.github.com>
2021-07-02 00:15:10 +0300

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,15 +1,19 @@
 import shutil
 import httpx
+import psutil
 import speedtest
 
 
 def disk_space() -> tuple:
     BytesPerGB = 1024 * 1024 * 1024
     total, used, free = shutil.disk_usage(".")
+    total_ram = psutil.virtual_memory().total
+    ram_used = psutil.virtual_memory().percent
     f_total = "%.2f GB" % (float(total) / BytesPerGB)
     f_used = "%.2f GB" % (float(used) / BytesPerGB)
     f_free = "%.2f GB" % (float(free) / BytesPerGB)
-    return f_total, f_used, f_free
+    f_total_ram = "%.2f GB" % (float(total_ram) / BytesPerGB)
+    return f_total, f_used, f_free, f_total_ram, ram_used
 
 
 def ip() -> str:
